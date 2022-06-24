ce54ee4804013682b2e6ee92f605dc77f7d77b81
Jabberwockiii <jacob4782241@gmail.com>
2022-06-24 13:45:03 -0400

    hope not too much

--- pythonScrapt/Scrape.py
+++ pythonScrapt/Scrape.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 number = 1
-for i in range(20):
+for i in range(100):
     response = requests.get("https://www.thispersondoesnotexist.com/image")
     file = open("./pythonScrapt/archived/"+str(i)+".png", "wb")
     file.write(response.content)
--- pythonScrapt/UploadS3.py
+++ pythonScrapt/UploadS3.py
@@ -1,4 +1,4 @@
 import boto3
 client = boto3.client('s3', region_name='us-east-1')
-for i in range():
+for i in range(100):
     client.upload_file("./pythonScrapt/archived/"+str(i)+'.png', 'scale1to10223b7f5797fd44bc9c60facc1a8348a6170543-environ', 'public/'+str(i)+'.png')
