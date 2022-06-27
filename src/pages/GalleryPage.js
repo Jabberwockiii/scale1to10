fff858c53f4b3405741f614b025f3be8f6a3f76d
Jabberwockiii <jacob4782241@gmail.com>
2022-06-27 15:32:51 -0400

    a stable version

--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useState, useEffect } from 'react';
-import { Storage } from 'aws-amplify'
+import { graphqlOperation, Storage } from 'aws-amplify'
 import { withAuthenticator} from '@aws-amplify/ui-react'
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
@@ -16,7 +16,9 @@ import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import {Link as RouterLink} from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import * as queries from '../graphql/queries';
+import { API } from 'aws-amplify';
+import Pagination from '@mui/material/Pagination';
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
@@ -29,12 +31,13 @@ function Copyright() {
     </Typography>
   );
 }
-//This is very dangerous 
 var imageDict = {};
+var titleDict = {};
 function Gallery() {
   //images hook
   const [images, setImages] = useState([]);
   const [open, setOpen] = useState(false);
+
   useEffect(() => {
     fetchImages()
   }, [])
@@ -42,11 +45,23 @@ function Gallery() {
       // Fetch list of images from S3
     Storage.configure({level: "public",})
     let s3images = await Storage.list('')
-    console.log("s3 images: ",s3images);
+    console.log("s3 images: ", s3images);
       // Get presigned URL for S3 images to display images in app
     s3images = await Promise.all(s3images.map(async image => {
       const signedImage = await Storage.get(image.key)
+      console.log(image)
       imageDict[signedImage] = image.key;
+      const post = await graphqlOperation(queries.getPost, { id: image.key });
+      const title = await API.graphql(post).then(res => {
+        return res.data.getPost.title;
+      }).catch(err => console.log(err));
+
+      if (title !== null) {
+        titleDict[signedImage] = title;
+      }
+      else{
+        titleDict[signedImage] = 'Judge me if you like me'
+      }
       return signedImage;
     }))
     setImages(s3images);
@@ -107,7 +122,7 @@ class PhotoList extends React.Component{
                   <RouterLink to={`/post/${imageDict[image]}`}>
                   <CardMedia component="img" md={{pt: '10%',}} image={image} alt="random"/>
                   </RouterLink>
-                    <CardContent> What do you think about this person?</CardContent>
+                    <CardContent>{titleDict[image]}</CardContent>
                   </Card>
                   <CardActions md={{display:'flex', pb:'1%'}} >
                       <RouterLink to={`/post/${imageDict[image]}`}
@@ -122,6 +137,9 @@ class PhotoList extends React.Component{
           </Container>
         </main>
         {/* Footer */}
+        <Box sx = {{display:'flex', justifyContent:'center'}}>
+          <Pagination count={10} color="secondary" />
+        </Box>
         <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
           <Typography variant="h6" align="center" gutterBottom>
             Footer
@@ -141,7 +159,6 @@ class PhotoList extends React.Component{
       );
     }
 }
-  //final return 
   return (<PhotoList />);
 }
-export default withAuthenticator(Gallery);
+export default Gallery;
--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -1,98 +1,147 @@
-import React, { useCallback, useState } from 'react';
-import InfiniteScroll from 'react-infinite-scroller';
-import parseLinkHeader from 'parse-link-header';
 // src/App.js
-import Typography from '@mui/material/Typography';
-import { API, SortDirection } from 'aws-amplify';
-import * as queries from '../graphql/queries';
-import * as mutations from '../graphql/mutations';
-import { graphqlOperation } from 'aws-amplify';
-import { TextField } from '@mui/material';
-import {v4 as uuid}from 'uuid';
+import React, { useState, useEffect } from 'react';
+import { Storage } from 'aws-amplify'
+import { withAuthenticator} from '@aws-amplify/ui-react'
 import Button from '@mui/material/Button';
-import CloseIcon from '@mui/icons-material/Close';
-import Dialog from '@mui/material/Dialog';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import { Auth } from 'aws-amplify';
-import {useParams} from 'react-router-dom';
-import { Divider, Avatar, Grid, Paper, Card } from "@material-ui/core";
-import Alert from '@mui/material/Alert';
-import CircularProgress from '@mui/material/CircularProgress';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import {Link as RouterLink} from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-async function fetchIssues(url) {
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: new Headers({
-      Accept: 'application/vnd.github.v3+json'
-    })
-  });
-
-  const links = parseLinkHeader(response.headers.get('Link'));
-  const issues = await response.json();
-
-  return {
-    links,
-    issues
-  };
-}
-
-export default function App() {
-  const [items, setItems] = useState([]);
-  const [nextPageUrl, setNextPageUrl] = useState(
-    'https://api.github.com/repos/facebook/react/issues'
-  );
-
-  const [fetching, setFetching] = useState(false);
-  const [comments, setComments] = useState([]);
-  const [nextToken, setNextToken] = useState(null);
-  
-  const fetchItems = useCallback(
-    async () => {
-      if (fetching) {
-        return;
-      }
-
-      setFetching(true);
-
-      try {
-        const { issues, links } = await fetchIssues(nextPageUrl);
-
-        setItems([...items, ...issues]);
-
-        if (links.next) {
-          setNextPageUrl(links.next.url);
-        } else {
-          setNextPageUrl(null);
-        }
-      } finally {
-        setFetching(false);
-      }
-    },
-    [items, fetching, nextPageUrl]
-  );
-  const hasMoreItems = !!nextPageUrl;
-  const loader = (
-    <div key="loader" className="loader">
-      Loading ...
-    </div>
-  );
+function Copyright() {
   return (
-    <InfiniteScroll
-      loadMore={fetchItems}
-      hasMore={hasMoreItems}
-      loader={loader}
-    >
-      <ul>
-        {items.map(item => (
-          <li key={item.id}>
-            <a href={item.url} target="_blank" rel="noopener">
-              {item.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </InfiniteScroll>
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
-};
+}
+//This is very dangerous 
+var imageDict = {};
+function Gallery() {
+  //images hook
+  const [images, setImages] = useState([]);
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    fetchImages()
+  }, [])
+  async function fetchImages() {
+      // Fetch list of images from S3
+    Storage.configure({level: "public",})
+    let s3images = await Storage.list('')
+    console.log("s3 images: ",s3images);
+      // Get presigned URL for S3 images to display images in app
+    s3images = await Promise.all(s3images.map(async image => {
+      const signedImage = await Storage.get(image.key)
+      imageDict[signedImage] = image.key;
+      return signedImage;
+    }))
+    setImages(s3images);
+  }
+const theme = createTheme();
+class PhotoList extends React.Component{
+    render(){
+      return(
+        <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <main>
+          {/* Hero unit */}
+          <Box
+            sx={{
+              bgcolor: 'background.paper',
+              pt: 0,
+              pb: 0,
+            }}
+          >
+            <Container maxWidth="sm">
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom = {true}
+                pt={8}
+              >
+                Rate your friends
+              </Typography>
+              <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                Here are some pictures you can rate and comment on.
+              </Typography>
+              <RouterLink to="/rule"
+              style = {{textDecoration:"none"}}>
+              <Button variant="h5" align="center" color="error" >
+                rating rule 
+              </Button>
+              </RouterLink>
+              <Stack
+                sx={{ pt: 4 }}
+                direction="row"
+                spacing={2}
+                justifyContent="center"
+              >
+              </Stack>
+            </Container>
+          </Box>
+          <Button variant="contained" color="error">
+            See more
+          </Button>
+          <Container sx={{ py: 10 }} maxWidth="lg">
+            {/* End hero unit */}
+            <Grid container spacing={5}>
+              {images.map((image) => (
+                <Grid item key={image} xs={12} sm={6} md={4}>
+                  <Card md={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                  <RouterLink to={`/post/${imageDict[image]}`}>
+                  <CardMedia component="img" md={{pt: '10%',}} image={image} alt="random"/>
+                  </RouterLink>
+                    <CardContent> What do you think about this person?</CardContent>
+                  </Card>
+                  <CardActions md={{display:'flex', pb:'1%'}} >
+                      <RouterLink to={`/post/${imageDict[image]}`}
+                      style={{ textDecoration: 'none' }}>
+                      <Button size="small">Rate</Button>
+                      </RouterLink>
+                      <Button size="small">Report</Button>
+                    </CardActions>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </main>
+        {/* Footer */}
+        <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="text.secondary"
+            component="p"
+          >
+            Something here to give the footer a purpose!
+          </Typography>
+          <Copyright />
+        </Box>
+        {/* End footer */}
+      </ThemeProvider>
+      );
+    }
+}
+  //final return 
+  return (<PhotoList />);
+}
+export default withAuthenticator(Gallery);
